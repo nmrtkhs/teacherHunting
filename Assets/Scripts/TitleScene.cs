e1832256e77a8fc30dd30a77411cae206550d43e
Nomura Takahisa <nmrtkhs@gmail.com>
2015-12-02 00:26:44 +0900

    Add Scenes

---
+++ Assets/Scripts/TitleScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnStartClick() {
+		Debug.Log ("hoge");
+	}
+
+	public void OnContinueClick() {
+	}
+}
