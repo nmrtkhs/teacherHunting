004be57099609572110729a95e8c9cc45a4bddfc
Shintaro Watanabe <shintaro.watanabe@gree.net>
2015-12-06 11:34:47 +0900

    modified player select method

--- Assets/Scripts/RegisterNewScene.cs
+++ Assets/Scripts/RegisterNewScene.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RegisterNewScene : MonoBehaviour {
 
+	private PhotonView myPv;
+	private GameObject characterButtonList;
+
 	// Use this for initialization
 	void Start () {
-	
+		myPv = this.GetComponent<PhotonView>();
+		characterButtonList = GameObject.Find("CharacterButtonList");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	[PunRPC]
+	void DisableCharacterId(int id)
+	{
+		GameObject targetCharacter = characterButtonList.transform.GetChild(id).gameObject;
+		targetCharacter.SetActive(false);
 	}
 
 	public void OnCharacterClick(int characterId) {
 		GameManager.instance.CharacterId = characterId;
+		myPv.RPC("DisableCharacterId", PhotonTargets.All, characterId);
 		Application.LoadLevel ("Game");
 	}
 }
