6b0a07c34b2a05d16f5627f207ba2ca8e8055f39
shin-taro <mashinta1226@gmail.com>
2016-01-11 13:13:41 +0900

    updated battle and register

--- Assets/Scripts/GameScene.cs
+++ Assets/Scripts/GameScene.cs
@@ -6,10 +6,14 @@ using System.Collections.Generic;
 public class GameScene : MonoBehaviour {
 
     public Sprite[] circleSprites;
+    public Sprite[] enemySprites;
+    public Sprite[] damagedEnemySprites;
 
 	private bool isStart = false;
 	private bool isInitialized = false;
     private bool isAnswering;
+    private Vector3 chargeGuageScale;
+    private RectTransform rtChargeGuage;
 	private PhotonView myPv;
 	private int bossHP;
 	private int lastCorrect = 0;
@@ -19,6 +23,7 @@ public class GameScene : MonoBehaviour {
     private float remainTime;
 
 	private GameObject playerAttacks;
+    private GameObject lightBeam;
 	private GameObject combo;
 	private GameObject hpGuage;
 	private GameObject startButton;
@@ -34,7 +39,7 @@ public class GameScene : MonoBehaviour {
     Text answerText2;
     Text answerText3;
     Text answerText4;
-
+    Image enemyImage;
     Button[] answerButtons;
 
     GameObject joinedPlayers;
@@ -50,6 +55,7 @@ public class GameScene : MonoBehaviour {
 		buttonText = new Text[4];
   
 		playerAttacks = GameObject.Find ("PlayerAttacks");
+        lightBeam = GameObject.Find ("LightBeam");
         combo = GameObject.Find("Combo");
 		hpGuage = GameObject.Find("HpGuage");
         panel = GameObject.Find("Panel");
@@ -60,7 +66,9 @@ public class GameScene : MonoBehaviour {
 		myPv = this.GetComponent<PhotonView>();
 		GameManager.instance.Score = 0;
 
+        rtChargeGuage = playerAttacks.transform.GetChild (0).GetComponent<RectTransform> ();
 		playerAttacks.SetActive(false);
+        lightBeam.SetActive (false);
 
 		questionManager = this.GetComponent<QuestionManager> ();
 		questionManager.LoadQuestion (GameManager.instance.SelectLevel);
@@ -89,9 +97,22 @@ public class GameScene : MonoBehaviour {
         remainTurn = 10;
         remainTime = 10.0f;
         isAnswering = false;
-        difficulty = GameManager.instance.difficulty + 1;
+        difficulty = GameManager.instance.difficulty;
         difficultyText = GameObject.Find ("DifficultyText").GetComponent<Text>();
-        difficultyText.text = "レベル" + difficulty.ToString ();
+        switch (difficulty) {
+        case 1:
+            difficultyText.text = "かんたん";
+            break;
+        case 2:
+            difficultyText.text = "ふつう";
+            break;
+        case 3:
+            difficultyText.text = "むずかしい";
+            break;
+        default:
+            difficultyText.text = "かんたん";
+            break;
+        }
 
         turnText = GameObject.Find ("TurnText").GetComponent<Text> ();
         turnText.text = "残り10ターン";
@@ -108,6 +129,9 @@ public class GameScene : MonoBehaviour {
             targetObject.SetActive(false);
         }
         GameManager.instance.Score = 0;
+
+        enemyImage = GameObject.Find ("Enemy").GetComponent<Image>();
+        enemyImage.sprite = enemySprites [GameManager.instance.SelectLevel];
     }
 	
 	// Update is called once per frame
@@ -132,11 +156,11 @@ public class GameScene : MonoBehaviour {
 
 		if (!isInitialized)
 		{
-			bossHP = 300 * PhotonNetwork.playerList.Length;
+			bossHP = 100 * PhotonNetwork.playerList.Length;
 			isInitialized = true;
 		}
 
-		if (bossHP - GameManager.instance.Score <= 0)
+		if (bossHP - GameManager.instance.Score <= 0 || remainTurn < 1)
 		{
 			AudioManager.Instance.StopBGM ();
 			Application.LoadLevel ("Result");
@@ -188,11 +212,8 @@ public class GameScene : MonoBehaviour {
 
     void StartQuestion()
     {
-        playerAttacks.SetActive(false);
-        //for (int i = 0; i < joinedPlayerObjectList.Count; i++) {
-        //    joinedPlayerObjectList [i].transform.GetChild (0).gameObject.SetActive (false);
-        //}
-
+        enemyImage.sprite = enemySprites [GameManager.instance.SelectLevel];
+        lightBeam.SetActive(false);
         questionText.text = "第" + questionIndex.ToString() + "問";
         answerNum = -1;
         Invoke ("SetQuestion", 1.0f);
@@ -229,6 +250,9 @@ public class GameScene : MonoBehaviour {
 
     void SetAttack()
     {
+        questionText.text = "";
+        playerAttacks.SetActive(true);
+        rtChargeGuage.localScale = new Vector3(0,1,1);
         int i = 0;
         foreach (GameObject target in characterDictionary.Values)
         {
@@ -268,10 +292,37 @@ public class GameScene : MonoBehaviour {
                 iTween.ScaleTo(target.transform.GetChild(0).gameObject, Vector3.one, 0);
                 target.transform.GetChild(0).gameObject.SetActive(false);
             }
-            playerAttacks.SetActive(true);
-            hpGuage.transform.localScale = new Vector3(1,(float)(bossHP - GameManager.instance.Score) / bossHP,1);
-            Invoke ("StartQuestion", 2.0f);
+
+            chargeGuageScale = new Vector3(0,1,1);
+
+            iTween.ValueTo(gameObject, iTween.Hash(
+                "from", chargeGuageScale,
+                "to", chargeGuageScale = new Vector3(correctCount / characterDictionary.Count,1,1),
+                "time", 1.0f,
+                "easeType", "easeInCubic",
+                "onupdate", "UpdateCharge",
+                "onupdatetarget", gameObject,
+                "oncomplete", "OnChargeComplete",
+                "oncompletetarget", gameObject
+            ));
         }
+    }
+
+    public void UpdateCharge()
+    {
+        rtChargeGuage.localScale = chargeGuageScale;
+    }
+
+    public void OnChargeComplete()
+    {
+        playerAttacks.SetActive (false);
+        if (correctCount > 0) {
+            lightBeam.SetActive (true);
+            AudioManager.Instance.PlaySE ("kougekion");
+            enemyImage.sprite = damagedEnemySprites [GameManager.instance.SelectLevel];
+        }
+        hpGuage.transform.localScale = new Vector3(1,(float)(bossHP - GameManager.instance.Score) / bossHP,1);
+        Invoke ("StartQuestion", 2.0f);
     }
 
 	public void onAnswerClick(int buttonNo) {
@@ -279,32 +330,7 @@ public class GameScene : MonoBehaviour {
         foreach (Button button in answerButtons) {
             button.image.color = Color.white;
         }
-
         answerButtons [buttonNo].image.color = new Color (0.5f, 1, 1, 1);
-
-//		if (!isStart) {
-//			return;
-//		}
-//
-//		if (questionManager.IsCorrectAnswer(buttonNo)) {
-//			GameManager.instance.Score += (30 + 30 * lastCorrect / 2);
-//			scoreText.text = GameManager.instance.Score.ToString ();
-//			myPv.RPC ("addScore", PhotonTargets.All, GameManager.instance.Score);
-//			myPv.RPC ("SetAttack", PhotonTargets.All, GameManager.instance.CharacterId, lastCorrect);
-//			difficulty++;
-//			if (difficulty > 3) {
-//				difficulty = 3;
-//			}
-//
-//			lastCorrect = 1;
-//		} else {
-//			difficulty--;
-//			if (difficulty < 1) {
-//				difficulty = 1;
-//			}
-//			lastCorrect = 0;
-//		}
-//		questionManager.SetQuestion(difficulty);
 	}
 
     public void CheckAnswer()
@@ -313,8 +339,10 @@ public class GameScene : MonoBehaviour {
         bool isCorrect = questionManager.IsCorrectAnswer (answerNum);
         if (isCorrect) {
             questionText.text = "正解";
+            AudioManager.Instance.PlaySE("seikai");
             myPv.RPC ("SetCorrect", PhotonTargets.All, GameManager.instance.CharacterId, difficulty - 1);
         } else {
+            AudioManager.Instance.PlaySE("fuseikai");
             questionText.text = "不正解";
         }
         foreach (Button button in answerButtons) {
